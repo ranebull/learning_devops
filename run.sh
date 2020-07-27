e150f2b1b486dcd8131f2d7685c031102de9e4aa
ranebull <ranebull@users.noreply.github.com>
2020-07-27 22:04:20 +0300

    Restore state

--- run.sh
+++ run.sh
@@ -25,7 +25,7 @@ if [ -f $HOMEWORK_RUN ]; then
 		-e DOCKER_HOST=unix:///tmp/docker.sock --cap-add=NET_ADMIN -p 33433:22 --privileged \
 		--device /dev/net/tun --name hw-test --network hw-test-net $DOCKER_IMAGE
 	# Show versions & run tests
-	docker exec hw-test bash -c 'echo -=Get versions=-; ansible --version; ansible-lint --version; packer version; terraform version; tflint --version; docker version; docker-compose --version; pwd; ls -al;'
+	docker exec hw-test bash -c 'echo -=Get versions=-; ansible --version; ansible-lint --version; packer version; terraform version; tflint --version; docker version; docker-compose --version'
 	docker exec -e USER=appuser -e BRANCH=$BRANCH hw-test $HOMEWORK_RUN
 
 	# ssh -i id_rsa_test -p 33433 root@localhost "cd /srv && BRANCH=$BRANCH $HOMEWORK_RUN"
