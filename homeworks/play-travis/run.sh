4e974f9fd84d61a633ae31d3de1f60a160ab09cd
ranebull <ranebull@users.noreply.github.com>
2020-07-27 21:51:24 +0300

    Update run.sh

--- homeworks/play-travis/run.sh
+++ homeworks/play-travis/run.sh
@@ -2,6 +2,6 @@
 EXIT_STATUS=0
 
 inspec exec learning_devops/homeworks/play-travis || EXIT_STATUS=$?
-(cd ./learning_devops/homeworks/play-travis && python3 -m unittest) || EXIT_STATUS=$?
+(cd ./learning_devops/homeworks/play-travis && pwd && ls -al && python3 -m unittest && echo "success") || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
