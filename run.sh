21861b09f942bcbb771cb6c6da855377ec115a0e
ranebull <ranebull@users.noreply.github.com>
2020-07-27 20:57:38 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 GROUP=2019-02
 BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH}
-HOMEWORK_RUN=./otus-homeworks/homeworks/$BRANCH/run.sh
-REPO=https://github.com/express42/otus-homeworks.git
+HOMEWORK_RUN=./learning_devops/homeworks/$BRANCH/run.sh
+REPO=https://github.com/ranebull/learning_devops.git
 DOCKER_IMAGE=express42/otus-homeworks
 
 echo GROUP:$GROUP
